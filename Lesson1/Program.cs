893461f54a5784ab1103af81b4533e7b94f71c94
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2023-02-10 22:03:50 +0300

    add lesson1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1
+
+/* bool useRandomNumber;
+int number = 0;
+string randomRequestAnswer;
+askToUseRandom();
+
+if (randomRequestAnswer.Equals("Y"))
+{
+    number = new Random().Next(1, 10);
+}
+if (randomRequestAnswer.Equals("N"))
+{
+    Console.WriteLine("Enter the number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+else
+{
+    Console.WriteLine("Wrong answer. Please type Y or N");
+    askToUseRandom();
+}
+
+int result = number * number;
+Console.WriteLine("Number = " + number);
+Console.WriteLine("Squared number = " + result);
+
+
+void askToUseRandom()
+{
+    Console.WriteLine("Do you want to use random number? (Y/N) :");
+    randomRequestAnswer = Console.ReadLine();
+} */
+
+
+
+//Задача 2
+
+/* Console.Write("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+if (firstValue == Math.Pow(secondValue, 2))
+
+{
+    Console.WriteLine("Чиcло " + firstValue + " является квадратом от " + secondValue);
+}
+else
+{
+    Console.WriteLine("Чиcло " + firstValue + " НЕ является квадратом от " + secondValue);
+} */
+
+
+
+//Задача 3
+
+/* Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * (-1);
+while (negativeN <= N)
+{
+    Console.WriteLine(negativeN);
+    negativeN++;
+} */
