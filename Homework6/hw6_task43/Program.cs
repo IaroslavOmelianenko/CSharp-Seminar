6531d4284e8bfca98a2ac52d15cae20898454a73
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2023-03-07 20:02:16 +0300

    fixed mistakes

--- Homework6/hw6_task43/Program.cs
+++ Homework6/hw6_task43/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double InputRequest (string pointName)
+double InputRequest(string pointName)
 {
     double point = 0;
     Console.WriteLine($"Enter point {pointName}: ");
@@ -9,25 +9,29 @@ double InputRequest (string pointName)
     return point;
 }
 
-double[] CountLineCrossPoint (double b1, double k1, double b2, double k2)
+double[] CountLineCrossPoint(double b1, double k1, double b2, double k2)
 {
     double[] crossPoint = new double[2];
-    crossPoint[0] = (b2-b1) / (k1-k2);
-    crossPoint[1] = k1 * (b2-b1) / (k1-k2) + b1;
+    crossPoint[0] = (b2 - b1) / (k1 - k2);
+    crossPoint[1] = k1 * (b2 - b1) / (k1 - k2) + b1;
     return crossPoint;
 }
 
-bool LinesCrossCheck (double b1, double k1, double b2, double k2)
+bool LinesCrossCheck(double b1, double k1, double b2, double k2)
 {
     bool linesCrossing = true;
-    if ((k1 == k2) && (b1 == b2)) Console.WriteLine("Lines are crossing");
+    if ((k1 == k2) && (b1 == b2))
+    {
+        linesCrossing = false;
+        Console.WriteLine("Lines are matching");
+    }
 
     else if (k1 == k2)
     {
         linesCrossing = false;
         Console.WriteLine("Lines are parallel");
-    } 
-return linesCrossing;
+    }
+    return linesCrossing;
 }
 
 double b1 = InputRequest("b1");
@@ -35,11 +39,13 @@ double k1 = InputRequest("k1");
 double b2 = InputRequest("b2");
 double k2 = InputRequest("k2");
 Console.WriteLine($"[ b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} ]");
+bool linesCrossing = (LinesCrossCheck(b1, k1, b2, k2));
 
-if (LinesCrossCheck(b1,k1,b2,k2)){
-    double[] crossPoint = CountLineCrossPoint(b1,k1,b2,k2);
+if (linesCrossing)
+{
+    double[] crossPoint = CountLineCrossPoint(b1, k1, b2, k2);
     Console.WriteLine($"Lines are crossing at [{crossPoint[0]}, {crossPoint[1]}]");
-} 
+}
 
 
 
