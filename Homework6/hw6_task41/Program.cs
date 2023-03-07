04f704c4f280b1826a6b504f14f1df8329439d51
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2023-03-07 19:39:19 +0300

    add homework 6

---
+++ Homework6/hw6_task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+int NumbersQuantityRequest()
+{
+    Console.WriteLine("Enter numbers quantity: ");
+    int numbersQuantity = Convert.ToInt32(Console.ReadLine());
+    return numbersQuantity;
+}
+
+int[] InputRequest(int numbersQuantity)
+{
+    int[] array = new int[numbersQuantity];
+    for (int i = 0; i < numbersQuantity; i++)
+    {
+        Console.WriteLine($"Enter number #{i + 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int NumbersOverZeroCounter(int[] array)
+{
+    int counter = 0;
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    return counter;
+}
+
+Console.WriteLine($"Numbers over zero quantity: {NumbersOverZeroCounter(InputRequest(NumbersQuantityRequest()))}");
+
---
+++ Homework6/hw6_task43/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double InputRequest (string pointName)
+{
+    double point = 0;
+    Console.WriteLine($"Enter point {pointName}: ");
+    point = Convert.ToInt32(Console.ReadLine());
+    return point;
+}
+
+double[] CountLineCrossPoint (double b1, double k1, double b2, double k2)
+{
+    double[] crossPoint = new double[2];
+    crossPoint[0] = (b2-b1) / (k1-k2);
+    crossPoint[1] = k1 * (b2-b1) / (k1-k2) + b1;
+    return crossPoint;
+}
+
+bool LinesCrossCheck (double b1, double k1, double b2, double k2)
+{
+    bool linesCrossing = true;
+    if ((k1 == k2) && (b1 == b2)) Console.WriteLine("Lines are crossing");
+
+    else if (k1 == k2)
+    {
+        linesCrossing = false;
+        Console.WriteLine("Lines are parallel");
+    } 
+return linesCrossing;
+}
+
+double b1 = InputRequest("b1");
+double k1 = InputRequest("k1");
+double b2 = InputRequest("b2");
+double k2 = InputRequest("k2");
+Console.WriteLine($"[ b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} ]");
+
+if (LinesCrossCheck(b1,k1,b2,k2)){
+    double[] crossPoint = CountLineCrossPoint(b1,k1,b2,k2);
+    Console.WriteLine($"Lines are crossing at [{crossPoint[0]}, {crossPoint[1]}]");
+} 
+
+
+
+
